3adf3ad40eaa02881728087c21db0c0ca3b1cf42
unknown <sargey7@gmail.com>
2022-03-31 14:21:50 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const dbConnect = require("./config/dbConnect");
+
+const authRoutes = require("./routes/auth");
+const analyticsRoutes = require("./routes/analytics");
+const categoryRoutes = require("./routes/category");
+const orderRoutes = require("./routes/order");
+const positionRoutes = require("./routes/position");
+
+const app = express();
+
+dbConnect();
+
+app.use(passport.initialize());
+
+app.use(morgan("dev"));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/analytics", analyticsRoutes);
+app.use("/api/category", categoryRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/position", positionRoutes);
+
+module.exports = app;
